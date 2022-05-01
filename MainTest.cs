bfa8af2e5123643aa612d3713e35f9d93f23320b
H0t-tub1 <99767366+H0t-tub1@users.noreply.github.com>
2022-05-01 16:25:55 -0600

    Made the thing that will do the thing

---
+++ MainTest.cs
@@ -0,0 +1,62 @@
+#region Header
+/*
+ * Title: Main Test to see if saving will work
+ */
+#endregion
+
+#region USING
+using System;
+using System.Collections.Generic;
+using "ADD REST"
+
+#endregion
+
+namespace MainSaver
+{
+    public partial class  System Saver
+    {
+        public MainSaver
+        {
+            InitalizeComponnt();
+        }
+        #region GLOBAL VARS
+        int Display = 0;
+        int Amount = 0;
+        string FileName = string.Empty;
+        #endregion
+        
+        #region EVENT HANDLERS
+        
+        #endregion
+        
+        #region CUSTOM METHODS
+        private void SaveAFile(Boolean addon)
+        {
+            if (!addon)
+            {
+                NumWrite(FileName);
+                ListWrite(FileName)
+                MessageBox.Show("Your ammount has been listed", "Save Done", MessageBoxButtons.OK);
+            }
+        }
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
